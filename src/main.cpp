c10155b47ce196f2c94af427719bb98e26aaefd6
Jens Behmenburg <jensbehmenburg@gmail.com>
2021-11-12 19:20:47 +0100

    feat: added WebSocketServer

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
+#include <WebSocketsServer.h>
+
 // create wifi_settings.h and insert:
 /*
 #define myWifiSsid "yourSsid"
@@ -15,7 +17,8 @@ const char* password = myWifiPassword;
 
 OV2640 cam;
 
-WebServer server(80);
+WebServer webServer(80);
+WebSocketsServer webSocketServer = WebSocketsServer(81);
 
 // handling multiple clients
 WiFiClient clients[2];
@@ -34,19 +37,19 @@ void handle_jpg_stream() {
   char buf[32];
   int s;
 
-  clients[clientsCounter] = server.client();
+  clients[clientsCounter] = webServer.client();
 
   clients[clientsCounter].write(HEADER, hdrLen);
   clients[clientsCounter].write(BOUNDARY, bdrLen);
-  clientsCounter++;
+  //clientsCounter++;
 
   while (true) {
     //for (int i = 0; i < (sizeof(clients)/sizeof(WiFiClient)); i++) {
-        //if (!clients[i].connected()) {
+        if (!clients[0].connected()) {
         //   clientsCounter--;
         //   clients[i] = NULL;
-        //   break;
-        //}
+           break;
+        }
         cam.makeFrameBuffer();
         s = cam.getSize();
         clients[0].write(CTNTTYPE, cntLen);
@@ -56,7 +59,8 @@ void handle_jpg_stream() {
         clients[0].write(BOUNDARY, bdrLen);
     //}    
 
-    server.handleClient();
+    webServer.handleClient();
+    webSocketServer.loop();
   }
 }
 
@@ -66,7 +70,7 @@ const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
 const int jhdLen = strlen(JHEADER);
 
 void handle_jpg() {
-  WiFiClient client = server.client();
+  WiFiClient client = webServer.client();
 
   if (!client.connected()) return;
   cam.makeFrameBuffer();
@@ -75,24 +79,83 @@ void handle_jpg() {
 }
 
 void handle_test() {
-  WiFiClient client = server.client();
+  WiFiClient client = webServer.client();
 
   if (!client.connected()) return;
-  Serial.println(server.arg("Test123")); //http://192.168.188.45/test?Test123=blablabla
+  Serial.println(webServer.arg("Test123")); //http://192.168.188.45/test?Test123=blablabla
 
 }
 
 void handleNotFound() {
   String message = "Server is running!\n\n";
   message += "URI: ";
-  message += server.uri();
+  message += webServer.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += (webServer.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
-  message += server.args();
+  message += webServer.args();
   message += "\n";
-  server.send(200, "text / plain", message);
+  webServer.send(200, "text / plain", message);
 }
+
+void handle_WS(uint8_t num, uint8_t * payload) {
+  // string to uint8_t -> "string".getBytes(); Maybe with string length
+  // uint8_t to string -> (char *) payload
+
+  String message = (char *)payload;
+  webSocketServer.sendTXT(num, message);
+}
+
+// is called when receiving any webSocket message
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  // num = id of cliend, up to 5!
+  // type = type of the message
+  // payload = data as raw bytes
+  // length = how many bytes are in the data
+
+  // which webSocket event?
+  switch(type) {
+    // new client has connected
+    case WStype_CONNECTED: {
+        IPAddress ip = webSocketServer.remoteIP(num);
+        Serial.printf("[%u] Connection from ", num);
+        Serial.println(ip.toString());
+        Serial.printf("Payload from %s; ", payload);
+        Serial.printf("Type from %u; ", type);
+        Serial.printf("Length from %u\n", length);
+      }
+      break;
+
+    // client has disconnected
+    case WStype_DISCONNECTED: {
+      IPAddress ip = webSocketServer.remoteIP(num);
+      Serial.print(ip.toString());
+      Serial.printf(" has disconnected!\n");
+
+      }
+      break;
+
+    // echo text message back to client
+    case WStype_TEXT: {
+      Serial.printf("[%u] Text: %s\n", num, payload);
+      webSocketServer.sendTXT(num, payload);
+      handle_WS(num, payload);
+    }
+    break;
+
+    // for everything else: do nothing
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    default:
+      break;
+  }
+}
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -117,14 +180,22 @@ void setup() {
   Serial.println("/capture");
   Serial.println("Test Send Link: http://192.168.188.45/test?Test123=blablabla");
 
-  // server addresses
-  server.on("/stream", HTTP_GET, handle_jpg_stream);
-  server.on("/capture", HTTP_GET, handle_jpg);
-  server.on("/test", HTTP_GET, handle_test); // https://techtutorialsx.com/2016/10/22/esp8266-webserver-getting-query-parameters/
-  server.onNotFound(handleNotFound);
-  server.begin();
+  // setup webServer
+  webServer.on("/stream", HTTP_GET, handle_jpg_stream);
+  webServer.on("/capture", HTTP_GET, handle_jpg);
+  webServer.on("/test", HTTP_GET, handle_test); // https://techtutorialsx.com/2016/10/22/esp8266-webserver-getting-query-parameters/
+  webServer.onNotFound(handleNotFound);
+  webServer.begin();
+
+  // setup webSocketServer server
+  webSocketServer.begin();
+  webSocketServer.onEvent(onWebSocketEvent);
 }
 
 void loop() {
-  server.handleClient();
+  // handle webServer data
+  webServer.handleClient();
+
+  // handle webSocket data
+  webSocketServer.loop();
 }
